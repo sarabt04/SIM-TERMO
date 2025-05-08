a9aaad2b20928001cf7b489546402118eced61d2
rosatrull <1669711@uab.cat>
2025-05-08 10:00:34 +0200

    Add files via upload

---
+++ Exercici2.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May  1 11:54:47 2025
+
+@author: rosatrullcastanyer
+"""
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+# Configuració per usar LaTeX
+mpl.rcParams['text.usetex'] = True
+mpl.rcParams['font.family'] = 'serif'
+mpl.rcParams['text.latex.preamble'] = r'\usepackage{amsmath}'
+
+# DADES
+#x = [-0.83876, -0.93039, -1.14351, -1.4147, -1.78798, -2.39035, -4.14162]
+x = [-0.83876, -0.93039, -1.03129, -1.14351, -1.26994, -1.4147, -1.58403, -1.78798, -2.04448, -2.39035, -2.92346, -4.14162]
+#y =  [-0.29169, -0.23319, -0.31471, -0.40048, -0.51083, -0.75502, -1.49165]
+y = [-0.26136, -0.32158, -0.34814, -0.32158, -0.35954, -0.40347, -0.45887, -0.543, -0.65393, -0.76572, -0.91629, -1.30933]
+
+# Càlcul de la regressió lineal
+coefficients, cov_matrix = np.polyfit(x, y, 1, cov = True)  # Ajust lineal de grau 1
+polynomial = np.poly1d(coefficients)
+m, n = coefficients
+
+m_err, n_err = np . sqrt ( np . diag ( cov_matrix ) )
+print(f'm = {m} ± {m_err}')
+print(f'n= {n} ± {n_err}')
+
+correlation_coefficient = np.corrcoef(x, y)[0, 1]
+r_squared = correlation_coefficient ** 2
+print(f'Coeficient de correlacio (R^2): {r_squared:.4f}')
+
+# n = ln(A) --> A = e^n
+A = np.e**n
+inc_A = np.e**n_err
+print(f'A = {A} ± {inc_A}')
+
+# Calcular els valors ajustats (recta de regresió)
+y_fit = polynomial(x)
+
+# Crear la gráfica
+plt.figure(figsize=(5, 4), dpi= 500)
+plt.errorbar(x, y, fmt='o', color='b', label='Punts trobats amb la simulació', markersize=4)
+plt.plot(x, y_fit, 'r--', label=f'Regressió lineal: $y = {coefficients[0]:.3f}x + {coefficients[1]:.3f}$')
+
+#plt.title(r'$\ln{(\rho_l - \rho_g)}$ en función de $\ln{\left(1 - \frac{T}{T_c}\right)}$')
+plt.xlabel(r'$\ln{\left(1 - \frac{T}{T_c}\right)}$')
+plt.ylabel(r'$\ln{(\rho_l - \rho_g)}$')
+plt.grid(False)
+plt.legend()
+
+plt.tick_params(axis='both', direction='out', which='both', top=True, right=True)
+
+plt.tight_layout()
+plt.show()
